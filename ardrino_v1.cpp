7f77b2da2d6e5ee1675054f74519fab393220e8e
Max <savagecm@users.noreply.github.com>
2016-10-03 12:09:14 +0800

    Update ardrino_v1.cpp

--- ardrino_v1.cpp
+++ ardrino_v1.cpp
@@ -1,13 +1,3 @@
-
-/** RF24Mesh_Example.ino by TMRh20
-
-   This example sketch shows how to manually configure a node via RF24Mesh, and send data to the
-   master node.
-   The nodes will refresh their network address as soon as a single write fails. This allows the
-   nodes to change position in relation to each other and the master node.
-*/
-
-
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
